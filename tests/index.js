7ae080ac8a53d3298a625090d821a96ca5f67970
meowbmw <wang.feng@partner.bmw.com>
2024-12-10 09:43:21 +0800

    refine cases; add mocha

--- tests/index.js
+++ tests/index.js
@@ -1,34 +1,50 @@
 import assert from "assert";
-import { abs, sum, memcpy, quicksort } from "../build/debug.js";
-// assert.strictEqual(abs(-10), 10);
-// assert.strictEqual(abs(2), 2);
-// assert.strictEqual(sum(2), 3);
-// assert.strictEqual(sum(3), 6);
-
+import { abs, sum } from "../build/debug.js";
 import loader from "@assemblyscript/loader"; 
 import { readFileSync } from 'fs'
 
-loader.instantiate(
-    readFileSync('./build/debug.wasm')
-).then(({ exports }) => {
-    let arrayPtr = exports.__pin(exports.__newArray(exports.Int32Array_ID, [
-        3, 4, 5, 6, 7, 8, 9
-    ]));
-    exports.quicksort(arrayPtr);
-    let wasmArray = exports.__getInt32Array(arrayPtr);
-    console.log("Sorted array:", wasmArray);
-    exports.__unpin(arrayPtr);
+it('test abs', function () {
+    assert.equal(abs(22), 22);
+    assert.equal(abs(-22), 22);
+  });
 
-    // memcpy test from here
-    let srcPtr=exports.__pin(exports.__newArray(exports.Int32Array_ID, [
-        3, 4, 5, 6, 7, 8, 9
-    ]));
+it('test sum', function () {
+    assert.equal(sum(2), 3);
+    assert.equal(sum(3), 6);
+  });
 
-    let dstPtr=exports.__pin(exports.__newArray(exports.Int32Array_ID, [
-        9, 9, 9, 9, 9, 9, 9
-    ]));
-    exports.memcpy(srcPtr,dstPtr);
-    dstPtr = exports.__getInt32Array(dstPtr);
-    console.log("Dst array", dstPtr);
-})
+it('test memcpy', function () {
+    loader.instantiate(
+        readFileSync('./build/debug.wasm')
+    ).then(({ exports }) => {
+        // memcpy test from here
+        let srcPtr=exports.__pin(exports.__newArray(exports.Int32Array_ID, [
+            3, 4, 5, 6, 7, 8, 9
+        ]));
+    
+        let dstPtr=exports.__pin(exports.__newArray(exports.Int32Array_ID, [
+            9, 9, 9, 9, 9, 9, 9
+        ]));
+        exports.memcpy(srcPtr,dstPtr);
+        dstPtr = exports.__getInt32Array(dstPtr);
+        assert.equal(dstPtr[0], 3);
+        assert.equal(dstPtr[1], 4);
+        assert.equal(dstPtr[2], 5);
+    })
+  });
 
+it('test quicksort', function () {
+    loader.instantiate(
+        readFileSync('./build/debug.wasm')
+    ).then(({ exports }) => {
+        let arrayPtr = exports.__pin(exports.__newArray(exports.Int32Array_ID, [
+            9,8,7,6,5,4,3
+        ]));
+        exports.quicksort(arrayPtr);
+        let wasmArray = exports.__getInt32Array(arrayPtr);
+        assert.equal(wasmArray[0], 3);
+        assert.equal(wasmArray[1], 4);
+        assert.equal(wasmArray[2], 5);
+        exports.__unpin(arrayPtr);
+    })
+  });
